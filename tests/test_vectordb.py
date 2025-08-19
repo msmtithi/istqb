3401d33cd7412c6d8d362d3fd49533df50cd1763
Ahmath-Gadji <ahmathgadji27@gmail.com>
2025-08-19 08:21:12 +0000

    ignore db and vdb

--- tests/test_vectordb.py
+++ tests/test_vectordb.py
@@ -1,15 +1,22 @@
-
 from openrag.components.indexer.vectordb import MilvusDB
+
 
 class TestMilvusDB:
     def test_complex_templating():
         partition = ["bob.localhost"]
         filter = dict(file_id="a3b2c1", custom_param=314)
         expr, params = MilvusDB._build_expr_template_and_params(partition, filter)
-        assert expr == "partition in {partition} and file_id == {file_id} and custom_param == {custom_param}"
+        assert (
+            expr
+            == "partition in {partition} and file_id == {file_id} and custom_param == {custom_param}"
+        )
 
         # Note how parameter values are not converted to str
-        assert params == {"partition": ["bob.localhost"], "file_id": "a3b2c1", "custom_param": 314}
+        assert params == {
+            "partition": ["bob.localhost"],
+            "file_id": "a3b2c1",
+            "custom_param": 314,
+        }
 
     def test_templating_no_filter():
         # If there is no filter, the search is run on every document
