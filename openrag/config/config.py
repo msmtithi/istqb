e9d59679d9ee4a9c2225b52ba23145f6b8e4078f
Ahmath-Gadji <ahmathgadji27@gmail.com>
2025-08-19 14:59:34 +0000

    use absolute path for directories

--- openrag/config/config.py
+++ openrag/config/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from hydra import compose, initialize_config_dir
 from omegaconf import OmegaConf
 
-CONFIG_PATH = os.environ.get("CONFIG_PATH", "/app/.hydra_config")
+CONFIG_PATH = Path(os.environ.get("CONFIG_PATH", "/app/.hydra_config")).resolve()
 
 
 def load_config(config_path=CONFIG_PATH, overrides=None) -> OmegaConf:
@@ -13,9 +13,12 @@ def load_config(config_path=CONFIG_PATH, overrides=None) -> OmegaConf:
 
     # TODO: I set the version base to 1.1 to silence the warning message, review how we want to handle versioning
     with initialize_config_dir(
-        config_dir=config_path, job_name="config_loader", version_base="1.1"
+        config_dir=str(config_path), job_name="config_loader", version_base="1.1"
     ):
         config = compose(config_name="config", overrides=overrides)
+
         config.paths.data_dir = Path(config.paths.data_dir).resolve()
+        config.paths.log_dir = Path(config.paths.log_dir).resolve()
+        config.paths.prompts_dir = Path(config.paths.prompts_dir).resolve()
 
         return config
